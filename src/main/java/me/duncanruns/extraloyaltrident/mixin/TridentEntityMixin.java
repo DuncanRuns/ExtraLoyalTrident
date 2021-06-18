7e430dd410829327effc125b8f58449656033acf
DuncanRL <59705125+DuncanRL@users.noreply.github.com>
2021-06-18 02:04:45 -0300

    format pog

--- src/main/java/me/duncanruns/extraloyaltrident/mixin/TridentEntityMixin.java
+++ src/main/java/me/duncanruns/extraloyaltrident/mixin/TridentEntityMixin.java
@@ -1,10 +1,8 @@
 package me.duncanruns.extraloyaltrident.mixin;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.entity.projectile.TridentEntity;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -15,15 +13,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(TridentEntity.class)
 public abstract class TridentEntityMixin extends PersistentProjectileEntity {
 
-    @Shadow private boolean dealtDamage;
+    @Shadow
+    private boolean dealtDamage;
 
     protected TridentEntityMixin(EntityType<? extends PersistentProjectileEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    @Inject(method="tick",at=@At("HEAD"))
-    private void loyalTickMixin(CallbackInfo info){
-        if(getPos().y <= -10){
+    @Inject(method = "tick", at = @At("HEAD"))
+    private void loyalTickMixin(CallbackInfo info) {
+        if (getPos().y <= -10) {
             this.dealtDamage = true;
         }
     }
